51e4c241abcabbfe22b9c65a256d1a10afae7d60
Edward Lambke <elambke@yahoo.com>
2014-12-08 23:00:18 -0800

    cordova added

---
+++ cordova/pitchandputt/platforms/android/CordovaLib/ant-build/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+/Users/elambke/Documents/COMP2052/pandp/cordova/pitchandputt/platforms/android/CordovaLib/ant-build/AndroidManifest.xml : \
+/Users/elambke/Documents/COMP2052/pandp/cordova/pitchandputt/platforms/android/CordovaLib/AndroidManifest.xml \
---
+++ cordova/pitchandputt/platforms/android/ant-build/classes.dex.d
@@ -0,0 +1,9 @@
+/Users/elambke/Documents/COMP2052/pandp/cordova/pitchandputt/platforms/android/ant-build/classes.dex : \
+/Users/elambke/Documents/COMP2052/pandp/cordova/pitchandputt/platforms/android/ant-build/classes/ca/derspatero/pitchandputt/BuildConfig.class \
+/Users/elambke/Documents/COMP2052/pandp/cordova/pitchandputt/platforms/android/ant-build/classes/ca/derspatero/pitchandputt/CordovaApp.class \
+/Users/elambke/Documents/COMP2052/pandp/cordova/pitchandputt/platforms/android/ant-build/classes/ca/derspatero/pitchandputt/R$attr.class \
+/Users/elambke/Documents/COMP2052/pandp/cordova/pitchandputt/platforms/android/ant-build/classes/ca/derspatero/pitchandputt/R$drawable.class \
+/Users/elambke/Documents/COMP2052/pandp/cordova/pitchandputt/platforms/android/ant-build/classes/ca/derspatero/pitchandputt/R$string.class \
+/Users/elambke/Documents/COMP2052/pandp/cordova/pitchandputt/platforms/android/ant-build/classes/ca/derspatero/pitchandputt/R$xml.class \
+/Users/elambke/Documents/COMP2052/pandp/cordova/pitchandputt/platforms/android/ant-build/classes/ca/derspatero/pitchandputt/R.class \
+/Users/elambke/Documents/COMP2052/pandp/cordova/pitchandputt/platforms/android/ant-build/dexedLibs/classes-5eaf53bb6caa4ecfd84cddd72ad7d63e.jar \
